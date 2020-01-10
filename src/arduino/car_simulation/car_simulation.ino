6ef3207066d5e9e17bfc43333540a29932c89f69
elliot-g <54964408+elliot-g@users.noreply.github.com>
2020-01-10 21:17:22 +0000

    Create car_simulation.ino

---
+++ src/arduino/car_simulation/car_simulation.ino
@@ -0,0 +1,132 @@
+
+// joystick pins
+const uint8_t x_joy_pin = A0;
+const uint8_t y_joy_pin = A1;
+const uint8_t brake_switch = 2;
+
+// distance sensor pins
+const uint8_t trig_pin = 12;
+const uint8_t echo_pin = 11;
+
+const uint8_t pot_pin = 3;
+
+// constants for determining joystick position
+const int left_lim = 200;
+const int right_lim = 550;
+
+const int up_lim = 3;
+const int down_lim = 2;
+
+/*
+	Setup loop runs once.
+	Readies some components.
+*/
+void setup() {
+  pinMode(trig_pin, OUTPUT);
+  pinMode(echo_pin, INPUT);
+  pinMode(brake_switch, INPUT);
+  Serial.begin(9600);
+  //attachInterrupt(digitalPinToInterrupt(brake_switch), brake, CHANGE);
+  delay(2000);  // uneeded.
+}
+
+// uneeded for now
+#define BRAKE 4
+#define RIGHT 1
+#define LEFT 2
+#define REV 3
+#define NORM 0
+
+/*
+	Loop runs infinitely.
+	Reads and sends values over Serial.
+*/
+void loop() {
+  int x_axis = analogRead(x_joy_pin);
+  int y_axis = analogRead(y_joy_pin);
+  uint8_t brake = digitalRead(brake_switch);
+
+  /*
+    Serial.print(x_axis);
+    Serial.print(",");
+    Serial.print(y_axis);
+    Serial.print(",");
+    Serial.print(brake);
+    Serial.print("\n");
+  */
+
+  //Serial.print("y = ");
+
+
+  if (y_axis > right_lim) {
+    // up
+    Serial.print(2);
+  } else if (y_axis < left_lim) {
+    // down (reversing)
+    float distance = get_distance();
+
+    if (distance < 15.0) {
+      brake = 1;
+      Serial.print(0);
+    } else {
+      Serial.print(1);
+    }
+  } else {
+    //normal pos
+    Serial.print(0);
+  }
+
+  Serial.print(",");
+  //Serial.print("x = ");
+
+  if (x_axis > right_lim) {
+    // right
+    Serial.print(2);
+  } else if (x_axis < left_lim) {
+    //left
+    Serial.print(1);
+  } else {
+    //neither -- normal pos
+    Serial.print(0);
+  }
+
+  Serial.print(",");
+  //Serial.print("b = ");
+
+  if (brake) {
+    Serial.print(1);
+  } else {
+    Serial.print(0);
+  }
+
+  Serial.print("\n");
+
+  //print_val("dist", dist);
+  delay(500);  // TODO sort out
+
+}
+
+/*
+	(Unused)
+*/
+void print_val(String what, int val) {
+  Serial.print(what);
+  Serial.print(": ");
+  Serial.println(val);
+  Serial.println();
+}
+
+
+/*
+   Uses the distance sensor.
+*/
+float get_distance() {
+  digitalWrite(trig_pin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig_pin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig_pin, LOW);
+
+  float dist = pulseIn(echo_pin, HIGH) / 58.0;
+  return (dist == 0 || dist > 400) ? -1 : dist;
+}
