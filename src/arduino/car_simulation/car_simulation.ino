e13d89cd05a970f3f6a5f160cf238e07bb129353
elliot-g <github@elliotgorman.com>
2020-01-15 05:55:15 -0500

    review comments

--- src/arduino/car_simulation/car_simulation.ino
+++ src/arduino/car_simulation/car_simulation.ino
@@ -2,9 +2,9 @@
 uint8_t switch_blink = 1;
 
 // joystick pins
-const uint8_t x_joy_pin = A0;
-const uint8_t y_joy_pin = A1;
-const uint8_t sw_joy_pin = 13;
+const uint8_t x_joy_pin = A0;  // x-axis
+const uint8_t y_joy_pin = A1;  // y-axis
+const uint8_t sw_joy_pin = 13; // button
 
 // brake button
 const uint8_t brake_switch = 4;
@@ -13,7 +13,9 @@ const uint8_t brake_switch = 4;
 const uint8_t trig_pin = 12;
 const uint8_t echo_pin = 11;
 
+// pin for the DC motor
 const uint8_t motor_pin = 10;
+// pin for the potentiometer
 const uint8_t pot_pin = A2;
 
 // constants for determining joystick position
@@ -39,6 +41,7 @@ void setup() {
 	pinMode(led_backup, OUTPUT);
 	pinMode(led_l, OUTPUT);
 
+	// setup the joystick button
 	pinMode(sw_joy_pin, INPUT);
 	digitalWrite(sw_joy_pin, HIGH);
 
@@ -51,20 +54,19 @@ void setup() {
 
 	Serial.begin(9600);
 
-	delay(2000); // uneeded.
+	delay(2000); // uneeded, but here for sync.
 }
-
-// int backup_duration = 500;
 
 /*
   Loop runs infinitely.
   Reads and sends values over Serial.
 */
-void loop() { // read the coordinates and direction of the joysticks position
+void loop() {
+	// read the coordinates and direction of the joysticks position
 	int x_axis = analogRead(x_joy_pin);
 	int y_axis = analogRead(y_joy_pin);
 
-	// read the position of the brake button
+	// read the brake button
 	uint8_t brake = digitalRead(brake_switch);
 
 	// set the motor speed to the potentiometer value
@@ -72,17 +74,16 @@ void loop() { // read the coordinates and direction of the joysticks position
 
 	// buzzer on when joystick button is pressed
 	if (!digitalRead(sw_joy_pin)) {
-		// buzz at 500 frequency
+		// buzz at 1047 freq.
 		tone(buzzer_pin, 1047, 500);
 	} // there doesn't seem to be a need for noTone();
 
 	// check of the joystick is in the UP position
 	if (y_axis > right_lim) {
-		// up
 		// get the value from the ultrasonic sensor
 		unsigned long distance = get_distance();
 
-		// is there an object within 15cm of the back of the car
+		// if there an object within 15cm of the back of the car
 		if (distance < 15.0) {
 			// too close, begin braking
 			// send brake signal
@@ -90,72 +91,65 @@ void loop() { // read the coordinates and direction of the joysticks position
 			Serial.print(0);
 			// turn the backup lights off
 			digitalWrite(led_backup, LOW);
-			// continue forwards
-		} else {
+		} else { // move forward
 			Serial.print(2);
 			digitalWrite(led_backup, LOW);
 		}
 
 		// check of the joystick is in the DOWN position
 	} else if (y_axis < left_lim) {
-		// down (reversing)
 		// get the value from the ultrasonic sensor
 		unsigned long distance = get_distance();
 
-		// is there an object within 15cm of the back of the car
+		// if there an object within 15cm of the back of the car
 		if (distance < 15.0) {
 			// too close, begin braking
 			brake = 1;
 			Serial.print(0);
 			// turn the backup lights off
 			digitalWrite(led_backup, LOW);
-		} else {
+		} else { // move backward
 			Serial.print(1);
 
-			// turn the buzzer on and off every 500ms
+			// turn the buzzer on and off every 500ms (using the blink flag)
 			if (switch_blink) {
 				tone(buzzer_pin, 1047, 500);
 			}
 			// turn on the backup lights
 			digitalWrite(led_backup, HIGH);
 		}
-		// car is not moving
-	} else {
+	} else { // car is not moving
 		// normal pos
 		Serial.print(0);
 		digitalWrite(led_backup, LOW);
 	}
-	// output for processing
+
+	// output for processing (csv)
 	Serial.print(",");
 
 	// check of the joystick is in the RIGHT position
 	if (x_axis > right_lim) {
-		// right
 		Serial.print(2);
 		// alternate the right led on and off, while the left led stays off
 		analogWrite(led_r, switch_blink ? 255 : switch_blink);
 		digitalWrite(led_l, LOW);
-
-		// check of the joystick is in the LEFT position
-	} else if (x_axis < left_lim) {
-		// left
+	} else if (x_axis <
+			   left_lim) { // check of the joystick is in the LEFT position
 		Serial.print(1);
 		// alternate the left led on and off, while the right led stays off
 		digitalWrite(led_r, LOW);
 		digitalWrite(led_l, switch_blink);
-
-		// if the car is not moving
-	} else {
-		// neither -- normal pos
+	} else { // neither -- normal pos
 		Serial.print(0);
 		// turn the rght and left leds off
 		digitalWrite(led_r, LOW);
 		digitalWrite(led_l, LOW);
 	}
-	// output for processing
+
+	// output for processing (csv)
 	Serial.print(",");
-	// if the brake signal is sent
-	if (brake) {
+
+	if (brake) { // if the brake signal is sent
 		Serial.print(1);
 		// turn on the brake leds
 		analogWrite(led_brake, 255);
@@ -164,7 +158,8 @@ void loop() { // read the coordinates and direction of the joysticks position
 		// turn off the brake lights
 		analogWrite(led_brake, LOW);
 	}
-	// output for processing
+
+	// output for processing (csv)
 	Serial.print("\n");
 
 	// ternary operator for switch_blink
@@ -182,7 +177,11 @@ void print_val(String what, int val) {
 	Serial.println();
 }
 
-// Uses the distance sensor.
+/*
+  Get the reading of the distance sensor.
+  @returns: -1 if distance is too large or too small,
+  or the distance read if not.
+*/
 unsigned long get_distance() {
 	// prepare the sensor
 	digitalWrite(trig_pin, LOW);
