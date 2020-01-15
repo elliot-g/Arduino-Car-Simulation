d0e8f59f4d1aa347e45c6be9555b208f7fb897ee
Poodleduff4 <59735578+Poodleduff4@users.noreply.github.com>
2020-01-15 10:39:31 -0500

    Update car_simulation.ino

--- src/arduino/car_simulation/car_simulation.ino
+++ src/arduino/car_simulation/car_simulation.ino
@@ -20,6 +20,7 @@ const uint8_t pot_pin = A2;
 const int left_lim = 200;
 const int right_lim = 600;
 
+
 // led pins
 const uint8_t led_r = 9;
 const uint8_t led_l = 8;
@@ -60,94 +61,117 @@ void setup() {
   Loop runs infinitely.
   Reads and sends values over Serial.
 */
-void loop() {
+void loop() {\
+	//read the coordinates and direction of the joysticks position
 	int x_axis = analogRead(x_joy_pin);
 	int y_axis = analogRead(y_joy_pin);
+	     
+	//read the position of the brake button
 	uint8_t brake = digitalRead(brake_switch);
-	int pot_val = analogRead(pot_pin) / 4;
-
+	     
+	//set the motor speed to the potentiometer value
 	analogWrite(motor_pin, analogRead(pot_pin) / 4);
-
-	Serial.print("sw_pin = ");
-	Serial.println(digitalRead(sw_joy_pin));
-	Serial.print("pot = ");
-	Serial.println(pot_val);
-
+	     
+	//buzzer on when joystick button is pressed
 	if (!digitalRead(sw_joy_pin)) {
+		//buzz at 500 frequency
 		tone(buzzer_pin, 1047, 500);
 	} // there doesn't seem to be a need for noTone();
 
+	//check of the joystick is in the UP position
 	if (y_axis > right_lim) {
 		// up
+		//get the value from the ultrasonic sensor
 		unsigned long distance = get_distance();
 
+		//is there an object within 15cm of the back of the car 
 		if (distance < 15.0) {
 			// too close, begin braking
+			//send brake signal
 			brake = 1;
 			Serial.print(0);
+			//turn the backup lights off
 			digitalWrite(led_backup, LOW);
+			//continue forwards
 		} else {
 			Serial.print(2);
 			digitalWrite(led_backup, LOW);
 		}
 
+	//check of the joystick is in the DOWN position
 	} else if (y_axis < left_lim) {
 		// down (reversing)
+		//get the value from the ultrasonic sensor
 		unsigned long distance = get_distance();
-
+		
+		//is there an object within 15cm of the back of the car
 		if (distance < 15.0) {
 			// too close, begin braking
 			brake = 1;
 			Serial.print(0);
+			//turn the backup lights off
 			digitalWrite(led_backup, LOW);
 		} else {
 			Serial.print(1);
-
+			
+			//turn the buzzer on and off every 500ms
 			if (switch_blink) {
 				tone(buzzer_pin, 1047, 500);
 			}
+			//turn on the backup lights
 			digitalWrite(led_backup, HIGH);
 		}
+	//car is not moving
 	} else {
 		// normal pos
 		Serial.print(0);
 		digitalWrite(led_backup, LOW);
 	}
-
+	//output for processing
 	Serial.print(",");
 
+	     //check of the joystick is in the RIGHT position
 	if (x_axis > right_lim) {
 		// right
 		Serial.print(2);
+		//alternate the right led on and off, while the left led stays off
 		analogWrite(led_r, switch_blink ? 255 : switch_blink);
 		digitalWrite(led_l, LOW);
+		
+		//check of the joystick is in the LEFT position
 	} else if (x_axis < left_lim) {
 		// left
 		Serial.print(1);
+		//alternate the left led on and off, while the right led stays off
 		digitalWrite(led_r, LOW);
 		digitalWrite(led_l, switch_blink);
+		
+		//if the car is not moving
 	} else {
 		// neither -- normal pos
 		Serial.print(0);
+		//turn the rght and left leds off
 		digitalWrite(led_r, LOW);
 		digitalWrite(led_l, LOW);
 	}
-
+//output for processing
 	Serial.print(",");
-
+//if the brake signal is sent
 	if (brake) {
 		Serial.print(1);
+		//turn on the brake leds
 		analogWrite(led_brake, 255);
 	} else {
 		Serial.print(0);
+		//turn off the brake lights
 		analogWrite(led_brake, LOW);
 	}
-
+//output for processing
 	Serial.print("\n");
 
+	     //ternary operator for switch_blink
 	switch_blink = (switch_blink ? 0 : 1);
-	// switch_blink = !switch_blink;
-	delay(500); // TODO sort out
+	delay(500);
 }
 
 /*
@@ -160,17 +184,17 @@ void print_val(String what, int val) {
 	Serial.println();
 }
 
-/*
-   Uses the distance sensor.
-   TODO convert to unsigned long.
-*/
+//Uses the distance sensor.
 unsigned long get_distance() {
+	//prepare the sensor
 	digitalWrite(trig_pin, LOW);
 	delayMicroseconds(2);
 	digitalWrite(trig_pin, HIGH);
 	delayMicroseconds(10);
 	digitalWrite(trig_pin, LOW);
 
+	//send and recieve the signal from the sensor
 	unsigned long dist = pulseIn(echo_pin, HIGH) / 58.0;
+	//return distance if it is in an acceptable range
 	return (dist == 0 || dist > 400) ? -1 : dist;
 }
